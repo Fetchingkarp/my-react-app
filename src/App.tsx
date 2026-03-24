dcda825bf2fbb820eee25027b7bcbee4dabcf03a
Fetchingkarp <michaelsharrison05@gmail.com>
2026-03-24 23:08:25 +1100

    placeholder login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,87 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import knife from './assets/knife.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount,] = useState(0);
+  const [message, setMessage,] = useState("nothing yet");
+  const handleClick = async () => {
+  try {
+    const res = await fetch("http://192.168.0.206:5000/api/hello");
+    const data = await res.json();
+    setMessage(data.message);
+  } catch (err) {
+    console.error(err);
+    setMessage("Error contacting backend");
+  }
+  };
 
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault(); // prevents page reload
+
+    console.log("Name:", name);
+    console.log("Password:", password);
+    const body = { name, password, }
+
+    try {
+      const res = await fetch("http://192.168.0.206:5000/api/auth/register", {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(body)
+      })
+      const data = await res.json();
+      console.log(data);
+      setMessage("User registered successfully!");
+    } catch (err) {
+      console.error(err);
+      setMessage("Failed to register user");
+    }
+  };
+  
+  const [loginName, setLoginName] = useState<string>("");
+  const [loginPassword, setLoginPassword] = useState<string>("");
+  const handleLoginSubmit = async (e: React.FormEvent) => {
+    e.preventDefault(); // prevents page reload
+
+    console.log("Name:", loginName);
+    console.log("Password:", loginPassword);
+    const body = { loginName, loginPassword, }
+    try {
+      const res = await fetch("http://192.168.0.206:5000/api/auth/login", {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(body)
+      })
+      const data = await res.json();
+      console.log(data);
+      setMessage("User logged in successfully!");
+    } catch (err) {
+      console.error(err);
+      setMessage("Failed to login user");
+    }
+  }
+
+  // MAIN PAGE STARTS HERE
   return (
     <>
+      <header>
+        <button onClick={handleClick} style={{ marginTop: "20px" }}>
+          Click Me
+        </button>
+        <p>{message}</p>
+      </header>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src={knife} className="base" width="170" height="179" alt="" />
         </div>
         <div>
           <h1>Get started</h1>
@@ -31,13 +99,31 @@ function App() {
 
       <div className="ticks"></div>
 
-      <section id="next-steps">
+      <section id="register">
         <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <h2>Register</h2>
+          <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            placeholder="Enter name"
+            value={name}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setName(e.target.value)
+            }
+          />
+        </div>
+
+        <div>
+          <input
+            placeholder="Enter password"
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
@@ -54,10 +140,29 @@ function App() {
           </ul>
         </div>
         <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
+          <h2>Log In</h2>
+          <form onSubmit={handleLoginSubmit}>
+        <div>
+          <input
+            placeholder="Enter name"
+            value={loginName}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setLoginName(e.target.value)
+            }
+          />
+        </div>
+
+        <div>
+          <input
+            placeholder="Enter password"
+            value={loginPassword}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setLoginPassword(e.target.value)
+            }
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
           <p>Join the Vite community</p>
           <ul>
             <li>
